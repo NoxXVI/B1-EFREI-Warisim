a70b40e30bb2e93941be7d8bc9e72a21e5865cab
Timothé Leroy <77456834+NoxXVI@users.noreply.github.com>
2024-11-26 20:15:17 +0100

    RangeList test

--- js/game.js
+++ js/game.js
@@ -2,15 +2,26 @@ import { changePlayer } from "../components/infoBar.js";
 import { initMenu } from "../components/gameMenu.js";
 import { setNewFactory, setNewCity } from "../components/footerMenu.js";
 import { initMap } from "./mapLoader.js";
-
+import { rangeTiles } from "./units.js";
 initMap();
 initMenu();
+
 // setNewFactory()
 
+
+// Test représentation entité
 const testTile = document.getElementById("6-5");
 const testUnitIMG = `<img
-        src="../assets/sprites/entity/infantery.svg"
-        class="sprite"
-        id="infantry1"
-      />`;
+src="../assets/sprites/entity/infantery.svg"
+class="sprite"
+id="infantry1"
+/>`;
 testTile.innerHTML = testUnitIMG;
+
+
+// Test porté d'attaque
+console.log(rangeTiles([], 5, [6,5]))
+for (let crds of rangeTiles([], 5, [6,5])) {
+    const tile = document.getElementById(`${crds[0]}-${crds[1]}`)
+    tile.style.backgroundColor = "red"
+}
---
+++ js/units.js
@@ -0,0 +1,31 @@
+export function rangeTiles(rangeList, range, startTileCoords) {
+  const startTile = document.getElementById(
+    `${startTileCoords[0]}-${startTileCoords[1]}`
+  );
+  const [x, y] = startTileCoords;
+
+  if (!startTile || startTile.classList.contains("river")) {
+    return;
+  }
+  if (range === 0) {
+    return;
+  }
+  if (x < 0 || y < 0) {
+    return;
+  }
+
+  // Comment comparer des Arrays d'après chatgpt
+  if (rangeList.some((coords) => coords[0] === x && coords[1] === y)) {
+    return;
+  }
+
+  rangeList.push([x, y]);
+  console.log(`Coords : ${startTileCoords}`);
+
+  rangeTiles(rangeList, range - 1, [x + 1, y]); // Droite
+  rangeTiles(rangeList, range - 1, [x, y + 1]); // Bas
+  rangeTiles(rangeList, range - 1, [x, y - 1]); // Haut
+  rangeTiles(rangeList, range - 1, [x - 1, y]); // Gauche
+
+  return rangeList;
+}
