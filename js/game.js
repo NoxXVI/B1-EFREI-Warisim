77959c3d2a5397fd52922a42ae403238f1b646b7
Timothé Leroy <77456834+NoxXVI@users.noreply.github.com>
2024-11-26 15:45:50 +0100

    Small menu fix + building button

--- js/game.js
+++ js/game.js
@@ -1,8 +1,16 @@
-import {changePlayer} from "../components/infoBar.js"
-import {initMenu} from "../components/gameMenu.js"
-import {setNewFactory, setNewCity} from "../components/footerMenu.js"
-import { initMap } from "./mapLoader.js"
+import { changePlayer } from "../components/infoBar.js";
+import { initMenu } from "../components/gameMenu.js";
+import { setNewFactory, setNewCity } from "../components/footerMenu.js";
+import { initMap } from "./mapLoader.js";
 
-initMap()
-initMenu()
+initMap();
+initMenu();
 // setNewFactory()
+
+const testTile = document.getElementById("6-5");
+const testUnitIMG = `<img
+        src="../assets/sprites/entity/infantery.svg"
+        class="sprite"
+        id="infantry1"
+      />`;
+testTile.innerHTML = testUnitIMG;
